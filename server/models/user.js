37582430d0fbec07e344fdf3008644c854e5bd5f
Kaone Mathuba <kmathuba@gmail.com>
2018-10-08 06:40:15 +0200

    Add POST /user/login route

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 const validator = require('validator');
 const jwt = require('jsonwebtoken');
 const _ = require('lodash');
+const bcrypt = require('bryptjs');
 
 var userSchema = new mongoose.Schema({
   email: {
@@ -70,6 +71,43 @@ UserSchema.statics.findByToken = function(token) {
   });
 
 };
+
+UserSchema.statics.findByCredentials = function(email, password) {
+  var User = this;
+
+  return User.findOne({email}).then((user) => {
+    if (!user) {
+      return Promise.reject();
+    }
+
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(password, user.password, (err, res) => {
+        if (res) {
+          resolve(user);
+        } else {
+          reject();
+        }
+      });
+    });
+
+  });
+};
+
+UserSchema.pre('save', function(next) {
+  var user = this;
+
+  if (user.isModified('password')) {
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(user.password, salt, (err, hash) => {
+        user.password = hash;
+        next();
+      });
+    }
+  } else {
+    next();
+  }
+
+});
 
 var User = mongoose.model('User', userSchema);
 
